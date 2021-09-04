8ce80df218bfc9353cdbd01d79cce08b8b1995b0
Nicolas <nicolasbarcellos95@gmail.com>
2021-09-03 23:56:01 -0300

    remove unused imports

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,41 +1,5 @@
-import React, { useState } from "react";
-import {
-  Flex,
-  Heading,
-  Avatar,
-  AvatarGroup,
-  Text,
-  Icon,
-  IconButton,
-  Table,
-  Thead,
-  Tbody,
-  Tr,
-  Th,
-  Td,
-  Divider,
-  Link,
-  Box,
-  Button,
-  Input,
-  InputGroup,
-  InputLeftElement,
-  VStack,
-} from "@chakra-ui/react";
-import {
-  FiHome,
-  FiPieChart,
-  FiDollarSign,
-  FiBox,
-  FiCalendar,
-  FiChevronDown,
-  FiChevronUp,
-  FiPlus,
-  FiCreditCard,
-  FiSearch,
-  FiBell,
-} from "react-icons/fi";
-import NavLink from "../components/Sidebar/NavLink";
+import { Flex } from "@chakra-ui/react";
+
 import Sidebar from "../components/Sidebar";
 import { Main } from "../components/Main";
 import { CartTransaction } from "../components/CartTransaction";
